65cf1781bd6262c25da9f12e16389f9ff3c4fff4
dorrrn <doan7tran@gmail.com>
2022-05-10 22:44:17 +0200

    added searchform

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import 'bulma/css/bulma.css';
 import foodsList from './foods.json';
 import FoodBox from './components/FoodBox';
 import AddFoodForm from './components/AddFoodForm';
+import './App.css';
 
 function App() {
   const [foods, setFoods] = useState(foodsList);
+  const [filteredFoods, setFilteredFoods] = useState('');
 
   const handleSubmit = (event, newFood) => {
     event.preventDefault();
@@ -13,22 +15,71 @@ function App() {
     setFoods(updateFood);
   };
 
+  const handleSearchInput = (event) => {
+    setFilteredFoods(event.target.value);
+  };
+
+  const handleDelete = (event) => {
+    setFoods(
+      foods.filter((foodsDelete) => {
+        return foodsDelete.name !== event;
+      })
+    );
+  };
+
   return (
     <>
       <div className="App">
+        <h1>Foods List</h1>
+        <hr />
         <div className="AddFood">
+          <h2>Add food</h2>
           <AddFoodForm handleSubmit={handleSubmit} />
         </div>
+        <hr />
+        <div className="Searchbar">
+          <h2>Search food</h2>
+          <input
+            value={filteredFoods}
+            placeholder="Search food"
+            type="text"
+            onChange={handleSearchInput}
+          />
+        </div>
 
-        {foods.map((food) => {
-          return (
-            <FoodBox
-              name={food.name}
-              calories={food.calories}
-              image={food.image}
-            />
-          );
-        })}
+        <div className="FoodsList">
+          {foods.length === 0 ? (
+            <h1>Oops! There is no more content to show</h1>
+          ) : (
+            foods
+              .filter((food) => {
+                const lowerFilter = filteredFoods.toLocaleLowerCase();
+                return food.name.toLocaleLowerCase().includes(lowerFilter);
+              })
+              .map((food, index) => {
+                return (
+                  <div key={food.name + index}>
+                    <FoodBox
+                      name={food.name}
+                      calories={food.calories}
+                      image={food.image}
+                      handleDelete={() => handleDelete(food.name)}
+                    />
+                  </div>
+                );
+              })
+          )}
+
+          {/* {foods.map((food) => {
+            return (
+              <FoodBox
+                name={food.name}
+                calories={food.calories}
+                image={food.image}
+              />
+            );
+          })} */}
+        </div>
       </div>
     </>
   );
